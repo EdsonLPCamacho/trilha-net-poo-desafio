66808fa946b82a4251369186d9b88f7102251d8c
Edson L P Camacho <123250293+EdsonLPCamacho@users.noreply.github.com>
2025-10-15 17:05:05 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        // Construtor que repassa todos os parâmetros para o construtor da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"iPhone: Instalando \"{nomeApp}\" via App Store...");
+        }
     }
 }
